6655c7aacc3bb1107614f1e7a6ddd423881e282f
Hemanth2227 <mattahemanth267@gmail.com>
2025-03-10 13:54:57 +0530

    Add files via upload

---
+++ SwitchPrograms.java
@@ -0,0 +1,90 @@
+public class SwitchPrograms {
+    public static void main(String[] args) {
+        double num1 = 10, num2 = 5;
+        char operator = '*';
+        int day = 3;       
+        int month = 7;       
+        // Simple Calculator
+        System.out.println("Simple Calculator");
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println(num1 + " * " + num2 + " = " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println(num1 + " / " + num2 + " = " + result);
+                } else {
+                    System.out.println("Error! Division by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+
+        // Day of the Week
+        System.out.println("\nDay of the Week");
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day number! Enter a number between 1 and 7.");
+        }
+
+        // Season Checker
+        System.out.println("\nSeason Checker");
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Fall");
+                break;
+            default:
+                System.out.println("Invalid month number! Enter a number between 1 and 12.");
+        }
+    }
+}
